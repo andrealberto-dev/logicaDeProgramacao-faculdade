3d1eafa3640f4f28bd8285c1bfc00d49d2bbfc50
andrelucas-dev <andrelucasvitoralberto@gmail.com>
2026-03-06 18:51:52 -0300

    06.03.2026

---
+++ rocketseat/ex001-magica.py
@@ -0,0 +1,18 @@
+import os
+
+os.system("cls")
+
+print("Olhe as cartas abaixo e escolhe uma: ")
+print("------------------------------------")
+print("Q♣ K♦ J♥ Q♥ J♣ K♠")
+input()
+
+os.system("cls")
+
+print("A carta que você pensou...")
+input()
+
+print("Desapareceu!")
+print()
+print("J♠ Q♦ J♦ Q♠ K♥")
+input()
