df1fb24034eb00de3e2199f154d00c3688b1c753
andrelucas-dev <andrelucasvitoralberto@gmail.com>
2026-03-11 19:25:21 -0300

    11.03.26

---
+++ rocketseat/ex008-jogoDeAdvinhar.py
@@ -0,0 +1,29 @@
+perguntas = [
+    ["Seu animal gosta de bananas", "macaco"]
+    ]
+
+print("-=-" * 10)
+print("Jogo de Adivinhação!")
+print("-=-" * 10)
+print()
+
+while True:
+    print("Pense em um animal...")
+
+    acertou = False
+    for pergunta in perguntas:
+        resposta = input(f"{pergunta[0]} (s/n): ")
+        if resposta.lower() == "s":
+            print(f"Você pensou em {pergunta[1]}!")
+            acertou = True
+            break
+
+    if not acertou:
+        animal = input("Desisto! Em qual animal você pensou? ")
+        novaPergunta = input("Qual pergunta você faria para diferenciar esse animal? ")
+        perguntas.append([novaPergunta, animal])
+
+    resposta = input("Quer jogar novamente? (s/n): ")
+    if resposta.lower() != "s":
+        print("Ok! Foi bom jogar com você!")
+        break
